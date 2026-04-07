94e517ea5cc9e4876c294d80a9cf4f8e863da7d8
Madhumitha <madhumithaprakash23@gmail.com>
2026-04-07 21:51:36 +0530

    fix backend error

--- todo-app/backend/server.js
+++ todo-app/backend/server.js
@@ -1,40 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+// ✅ CORS
 app.use(cors({
   origin: "*"
 }));
 
-const app = express();
-app.use(cors());
 app.use(express.json());
 
-// 🔥 MongoDB connection (PASTE YOUR URL)
-mongoose.connect("mongodb://madhu:Madhu123@ac-f1qnnit-shard-00-00.garkijc.mongodb.net:27017,ac-f1qnnit-shard-00-01.garkijc.mongodb.net:27017,ac-f1qnnit-shard-00-02.garkijc.mongodb.net:27017/?ssl=true&replicaSet=atlas-fqq34u-shard-0&authSource=admin&appName=Cluster0")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
-
-// 🔥 Model
+// ✅ Model
 const Task = mongoose.model("Task", {
   title: String,
   status: { type: String, default: "pending" }
 });
 
-// 🔥 Routes
+// ✅ Routes
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
 
-// Create
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
   const saved = await task.save();
   res.json(saved);
 });
 
-// Read
-app.get("/tasks", async (req, res) => {
-  const tasks = await Task.find();
-  res.json(tasks);
-});
-
-// Delete
 app.delete("/tasks/:id", async (req, res) => {
   await Task.findByIdAndDelete(req.params.id);
   res.json({ message: "Deleted" });
@@ -44,9 +38,19 @@ app.put("/tasks/:id", async (req, res) => {
   const updated = await Task.findByIdAndUpdate(
     req.params.id,
     req.body,
-    { new: true }   // ⭐ VERY IMPORTANT
+    { new: true }
   );
   res.json(updated);
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// ✅ MongoDB + Server START
+mongoose.connect("mongodb://madhu:Madhu123@ac-f1qnnit-shard-00-00.garkijc.mongodb.net:27017,ac-f1qnnit-shard-00-01.garkijc.mongodb.net:27017,ac-f1qnnit-shard-00-02.garkijc.mongodb.net:27017/?ssl=true&replicaSet=atlas-fqq34u-shard-0&authSource=admin&appName=Cluster0")
+.then(() => {
+  console.log("MongoDB Connected");
+
+  app.listen(5000, () => {
+    console.log("Server running on port 5000");
+  });
+
+})
+.catch(err => console.log(err));
