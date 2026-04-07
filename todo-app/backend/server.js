655aac33fb021296f7890ae99d03956ce0da1c11
Madhumitha <madhumithaprakash23@gmail.com>
2026-04-07 21:44:24 +0530

    fixed cors

--- todo-app/backend/server.js
+++ todo-app/backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
+app.use(cors({
+  origin: "*"
+}));
 
 const app = express();
 app.use(cors());
